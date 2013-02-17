f1eafb6cdab6c5c871ec86700cb247e5fe32cc9b
Chris Mather <mather.chris@gmail.com>
2013-02-16 17:37:52 -0800

    custom annotation fn

--- spark-annotation-functions.js
+++ spark-annotation-functions.js
@@ -1,39 +1,44 @@
 if (Meteor.isClient) {
 
-  /* plain html with no annotations */
-  function plainHtml() {
-    return Template.greetingButton();
-  }
-
-  /* show what annotated html looks like. step 1 in materialization process */
-  function annotatedHtml() {
-    var renderer = new Spark._Renderer;
-    var html = Spark._currentRenderer.withValue(
-      renderer,
-      Template.greetingButton
-    );
-    return html;
-  }
-
-  /* this is exactly what Spark.render does except the materialize method
-   * creates the annotated html first and then does the materialization all
-   * in one step. We've broken it out for demonstration
-  */
-  function materializedHtml() {
-    var renderer = new Spark._Renderer;
-    var materializedHtmlFrag = renderer.materialize(Template.greetingButton);
-    return materializedHtmlFrag;
-  }
-
-  /* same as document.body.appendChild(Spark.render(Template.greetingButton)) */
-  function appendTemplateToBody() {
-    document.body.appendChild(materializedHtml());
-  }
-
-  /* define a simple event handler on the greetingButton template */
+  /* template event handlers */
   Template.greetingButton.events({
     "click button": function (e, tmpl) {
       console.log("clicked!");
     }
   });
+
+
+  /* custom annotation */
+  function customAnnotation (htmlFunc) {
+    var renderer = Spark._currentRenderer.get();
+    var html;
+
+    var materializeFunc = function (liveRange) {
+      console.log("Now parsing the annotations is done and we're materializing");
+    };
+
+    if (!renderer)
+      return htmlFunc();
+
+    html = renderer.annotate(
+      htmlFunc(),
+      "chris-custom-annotation",
+      materializeFunc
+    );
+
+    console.log(html);
+    return html;
+  }
+
+  function customRender () {
+    return customAnnotation(function () {
+      return "<div>Hello</div>";
+    });
+  }
+
+  Meteor.startup(function () {
+    document.body.appendChild(Spark.render(customRender));  
+  });
+
+
 }
